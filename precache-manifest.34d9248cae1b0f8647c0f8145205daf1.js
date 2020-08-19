e2af1f466448747f8cb237b03d971d33fc5b4d85
nadeemkhan6993 <nadeemkhan6993@gmail.com>
2020-08-19 18:46:13 +0530

    Updates

---
+++ precache-manifest.34d9248cae1b0f8647c0f8145205daf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3497fa0ab2b3aa232c043198f58168",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "e88c4dbf84245efbdca5",
+    "url": "/Todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cabf7eb3c10d035e1a38",
+    "url": "/Todo/static/js/2.36f4692c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.36f4692c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88c4dbf84245efbdca5",
+    "url": "/Todo/static/js/main.c7336fd0.chunk.js"
+  },
+  {
+    "revision": "54ae61dc4bceeec44752",
+    "url": "/Todo/static/js/runtime-main.5ba529eb.js"
+  }
+]);
