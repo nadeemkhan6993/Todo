eb4ecfdb09ea97e8252bf2119506ea87c0e4659c
nadeemkhan6993 <nadeemkhan6993@gmail.com>
2020-08-19 18:44:16 +0530

    deploy my react Todo app to github

---
+++ src/component/buglist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Buglist extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is my new bug list</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default Buglist;
